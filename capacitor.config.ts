62fdb3f78d2f8f359db74c18f2d10f04999ffe1b
Kleber <klebianemarcia.silva@gmail.com>
2026-06-15 13:04:29 -0300

    Adiciona projeto Android Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gestorfinanceiro.km',
+  appName: 'gestor-km-epp-novo-2026',
+  webDir: 'dist'
+};
+
+export default config;
